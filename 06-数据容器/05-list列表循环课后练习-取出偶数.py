cfa4b789c70e9a0035b3e1aeb1b2aee389950289
Wang1113Coder <18752153503@163.com>
2024-01-04 20:28:20 +0800

    Day7

---
+++ 06-数据容器/05-list列表循环课后练习-取出偶数.py
@@ -0,0 +1,20 @@
+"""
+演示list列表循环课后练习:取出偶数
+使用while循环和for循环个操作一次
+"""
+mylist1=[1,2,3,4,5,6,7,8,9,10]
+mylist2=[]
+
+# while循环
+# i=0
+# while i<len(mylist1):
+#     if mylist1[i]%2==0:
+#         mylist2.append(mylist1[i])
+#     i+=1
+# print(mylist2)
+
+# for循环
+# for i in mylist1:
+#     if i%2==0:
+#         mylist2.append(i)
+# print(mylist2)
