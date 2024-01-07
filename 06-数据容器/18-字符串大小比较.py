237ca75e93e29095b4a6923f4adfa2194da3a1ef
Wang1113Coder <18752153503@163.com>
2024-01-07 10:11:00 +0800

    Day10

---
+++ 06-数据容器/18-字符串大小比较.py
@@ -0,0 +1,12 @@
+"""
+演示字符串大小比较
+"""
+
+# abc比较abd
+print(f"abc大于abd,结果:{'abd'>'abc'}")
+# a比较ab
+print(f"ab大于a,结果:{'ab'>'a'}")
+# a比较A
+print(f"a大于A,结果:{'a'>'A'}")
+# key1比较key2
+print(f"key2大于key1,结果:{'key2'>'key1'}")
